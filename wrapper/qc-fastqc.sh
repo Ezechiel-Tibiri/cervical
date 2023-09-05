e7b27626b72d7dc97402ca306f981d094205049d
ianlucas07 <ianlucaskabore@gmail.com>
2023-09-05 22:26:17 +0000

    Control quality script validated

--- wrapper/qc-fastqc.sh
+++ wrapper/qc-fastqc.sh
@@ -6,7 +6,8 @@ QC_DIR="~/Documents/2023/Ian/RESULTS/QC"
 mkdir -p $QC_DIR
 # This script concern the "quality control"
 cd $QC_DIR
-fastqc $DIR_FASTQ/*.fastq.gz --threads 8
+# This command make quality control by using 12 core and simultanatly report the evolution of the script running thank's to " > sdout .log 2>&1". The path that leads to the data has been associated to the variable "DIR_FASTQ"
+fastqc $DIR_FASTQ/*.fastq.gz --threads 12 > sdout.log 2>&1
 
 echo "fastqc run successfully"
 
