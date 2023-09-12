0197b5ee0723595f57977dbd4d29596ae18674a2
Ezechiel B. TIBIRI <e.tibiri@agshare.today>
2023-09-12 07:36:30 +0000

    mapping script validation

--- wrapper/bwa.sh
+++ wrapper/bwa.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-conda activate bwa-env
-# Activation of bwa environnement. Which contain all the application that will be used for this step
+# Define working directories
+BWA_DIR=~/Documents/2023/Ian/RESULTS/BWA
+DIR_FASTQ=/home/lucas/DATA
 
-wget "downloading link of human Refseq" > Refseq.zip
-# Download human Refseq in format fasta and put it in the file "Refseq.zip"
+# Ensure the BWA directory exists or create it
+mkdir -p "$BWA_DIR"
 
-bwa index Refseq.zip
-#Index le fichier "Refseq.zip" comme référence
+# Activate the conda environment if needed (uncomment this line if it's required)
+#conda activate bwa-env
 
-BWA_DIR="~/Documents/2023/Ian/RESULTS/BWA"
-mkdir -p $BWA_DIR
-# work directory 
+# Download the genomic file if it doesn't exist
+if [ ! -f "GCF_000001405.40_GRCh38.p14_genomic.fna.gz" ]; then
+    wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+fi
 
-cd $BWA_DIR
+# Decompress the genomic file if needed
+if [ -f "GCF_000001405.40_GRCh38.p14_genomic.fna.gz" ]; then
+    gunzip GCF_000001405.40_GRCh38.p14_genomic.fna.gz
+fi
 
-bwa mem -t 8 Refseq.zip $DIR_FASTQ/*.fastq.gz | samtools sort -o $BWA_DIR/bwa.sorted.bam
-#"bwa mem" permit the mapping, "samtool" permit to convert the file obtained in extension ".sam" after mapping, in a file ".bam"; which use less stockage space 
+# Index the genome with BWA
+bwa index GCF_000001405.40_GRCh38.p14_genomic.fna
 
-multiqc
-#multiqc will do a quality control in all the file in the directory $BWA_DIR. This repertory must contain only the file "bwa.sorted.bam
+# Change to the BWA directory
+cd "$BWA_DIR"
+
+# Map the FASTQ files with BWA and sort the results using samtools
+bwa mem -t 8 $DIR_FASTQ/*.fastq.gz | samtools sort -o SRR966.sorted.bam
